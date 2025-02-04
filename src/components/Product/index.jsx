4b81a79aa1e29c242ba5a1dc21170d048a79e2f0
Kirti Padhi <bigbinary@BigBinarys-MacBook-Pro-4.local>
2025-02-04 20:15:05 +0530

    Added product listing page

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,21 +1,25 @@
 import { useEffect, useState } from "react";
 
 import productsApi from "apis/products";
-import { Typography, Spinner } from "neetoui";
+import { Header, PageNotFound, PageLoader } from "components/commons";
+import { Typography } from "neetoui";
 import { append, isNotNil } from "ramda";
+import { useParams } from "react-router-dom";
 
 import Carousel from "./Carousel";
 
 const Product = () => {
+  const { slug } = useParams();
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
   const [product, setProduct] = useState({});
 
   const fetchProduct = async () => {
     try {
-      const product = await productsApi.show();
+      const product = await productsApi.show(slug);
       setProduct(product);
-    } catch (error) {
-      console.log("An error occurred:", error);
+    } catch {
+      setIsError(true);
     } finally {
       setIsLoading(false);
     }
@@ -29,20 +33,13 @@ const Product = () => {
   const totalDiscounts = mrp - offerPrice;
   const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
 
-  if (isLoading) {
-    return (
-      <div className="flex h-screen w-full items-center justify-center">
-        <Spinner />
-      </div>
-    );
-  }
+  if (isError) return <PageNotFound />;
+
+  if (isLoading) return <PageLoader />;
 
   return (
     <div className="px-6 pb-6">
-      <div>
-        <Typography className="py-2 text-4xl font-semibold">{name}</Typography>
-        <hr className="border-2 border-black" />
-      </div>
+      <Header title={name} />
       <div className="mt-6 flex gap-4">
         <div className="w-2/5">
           <div className="flex justify-center gap-16">
